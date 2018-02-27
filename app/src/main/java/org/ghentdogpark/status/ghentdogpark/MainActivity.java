66a8519855bf7b084930837cbf091ad13245cce4
Wouter Deconinck <wdconinc@gmail.com>
2018-02-26 20:53:40 -0500

    Initial commit

---
+++ app/src/main/java/org/ghentdogpark/status/ghentdogpark/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ghentdogpark.status.ghentdogpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
